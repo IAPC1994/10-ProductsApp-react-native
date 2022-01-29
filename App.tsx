6ca22e4fd4df3e34eb031aa08107c7494bccf9bf
Ivan Panussis <ivan.panussis.c@gmail.com>
2022-01-29 05:52:43 -0300

    Context, reducer and interface completed for http requests for login and register

--- App.tsx
+++ App.tsx
@@ -3,13 +3,25 @@ import React from 'react';
 import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/router/StackNavigator';
+import { AuthProvider } from './src/context/AuthContext';
 
 LogBox.ignoreAllLogs();
+
+
+const AppState = ({ children}: { children: JSX.Element | JSX.Element[] }) => {
+  return(
+    <AuthProvider>
+      { children }
+    </AuthProvider>
+  )
+}
 
 const App = () => {
     return(
       <NavigationContainer>
-        <StackNavigator />
+        <AppState>
+          <StackNavigator />
+        </AppState>
       </NavigationContainer>
     );
 };
